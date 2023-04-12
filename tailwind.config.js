0a49a17be3b0f514d67c208ea3eeaf20cccd6374
mayazavrik <mayazavrik@gmail.com>
2023-04-13 00:50:23 +0300

    final final version

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./index.html", "./**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main': 'hsl(36, 100%, 99%)',
+        'hover': 'hsl(35, 77%, 62%)',
+      }
+    },
   },
   plugins: [],
 };
