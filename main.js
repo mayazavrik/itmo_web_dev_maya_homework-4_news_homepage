da1de7da6c3213bd70e5930cada6bdd4594e23d1
mayazavrik <127432774+mayazavrik@users.noreply.github.com>
2023-04-13 16:55:24 +0300

    мобильное меню везде

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 import './style.css';
+const navbar = document.querySelector("nav");
+const openMenu = document.getElementById("menu-button");
+const closeMenu = document.getElementById("menu-close");
+
+openMenu.addEventListener("click", () => {
+    navbar.classList.add("open");
+});
+closeMenu.addEventListener("click", () => {
+    navbar.classList.remove("open");
+});
